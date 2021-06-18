31088a756a5cf9bf8436eec1604bc453c6b89ebe
JithinVijayakumar <84662663+JithinVijayakumar@users.noreply.github.com>
2021-06-18 20:49:30 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -11,3 +11,36 @@ sum = sum + m[i];
 float avg = (float)sum/a;
 return avg;
 }  
+
+int factors(int a, int m[])
+{
+int count =0;
+int z=0;
+
+
+for(int i = 2;i<=a;i++)
+{
+count =0;
+for (int j=1;j<=i;j++)
+{
+
+if ((i%j)==0)
+count = count +1;
+}
+if(count ==2)
+
+while(a%i==0)
+{
+m[z]=i;
+z=z+1;
+a=a/i;
+
+}
+
+
+
+}
+ ret[]=m[];
+ return(z);
+
+}
