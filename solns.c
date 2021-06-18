be39d1c13e941d7959b9fbf88b41fb45a3219918
JithinVijayakumar <84662663+JithinVijayakumar@users.noreply.github.com>
2021-06-18 19:14:58 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -13,3 +13,14 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+  
+float average(int m[], int a)
+{
+int sum = 0;
+
+for(int i=0;i<a;i++)
+sum = sum + m[i];
+
+float avg = sum/a;
+return avg;
+}  
